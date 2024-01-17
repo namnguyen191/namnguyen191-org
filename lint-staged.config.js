e40cfa3fcbe6fdc75ca6250b084df310c3803e11
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-01-16 21:42:13 -0500

    feat: adjust lint-stage to only lint changed files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   'package.json': ['pnpm typecheck:all', 'pnpm lint:all', 'pnpm build:all'],
   '{apps,libs,tools}/**/*.{ts,tsx}': 'pnpm type-check:affected',
   '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json,html}': [
-    'pnpm lint:affected',
+    (files) => `pnpm nx affected:lint --files=${files.join(',')}`,
     (files) => `pnpm nx format:write --files=${files.join(',')}`,
   ],
 };
