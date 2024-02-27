e6157d2a39ee9352583a45640df9b032c4223e19
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-02-26 22:22:49 -0500

    feat: the wild oasis add header

--- apps/the-wild-oasis/src/app/features/authentication/UserAvatar.tsx
+++ apps/the-wild-oasis/src/app/features/authentication/UserAvatar.tsx
@@ -1,22 +1,45 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { UserMetadata } from '@supabase/supabase-js';
+import { ReactElement } from 'react';
 import styled from 'styled-components';
 
-// const StyledUserAvatar = styled.div`
-//   display: flex;
-//   gap: 1.2rem;
-//   align-items: center;
-//   font-weight: 500;
-//   font-size: 1.4rem;
-//   color: var(--color-grey-600);
-// `;
+import { useCurrentUser } from './authQueryHooks';
 
-// const Avatar = styled.img`
-//   display: block;
-//   width: 4rem;
-//   width: 3.6rem;
-//   aspect-ratio: 1;
-//   object-fit: cover;
-//   object-position: center;
-//   border-radius: 50%;
-//   outline: 2px solid var(--color-grey-100);
-// `;
+const StyledUserAvatar = styled.div`
+  display: flex;
+  gap: 1.2rem;
+  align-items: center;
+  font-weight: 500;
+  font-size: 1.4rem;
+  color: var(--color-grey-600);
+`;
+
+const Avatar = styled.img`
+  display: block;
+  width: 4rem;
+  width: 3.6rem;
+  aspect-ratio: 1;
+  object-fit: cover;
+  object-position: center;
+  border-radius: 50%;
+  outline: 2px solid var(--color-grey-100);
+`;
+
+export const UserAvatar = (): ReactElement => {
+  const { user } = useCurrentUser();
+
+  if (!user) {
+    return <span>No user</span>;
+  }
+
+  const { fullName, avatar } = user.user_metadata as UserMetadata & {
+    fullName: string;
+    avatar?: string;
+  };
+
+  return (
+    <StyledUserAvatar>
+      <Avatar src={avatar ?? 'default-user.jpg'} alt={`avatar of ${fullName}`} />
+      <span>{fullName}</span>
+    </StyledUserAvatar>
+  );
+};
--- apps/the-wild-oasis/src/app/ui/Header.tsx
+++ apps/the-wild-oasis/src/app/ui/Header.tsx
@@ -1,18 +1,25 @@
 import { FC } from 'react';
 import styled from 'styled-components';
 
-import { LogoutButton } from '../features/authentication/LogoutButton';
+import { UserAvatar } from '../features/authentication/UserAvatar';
+import { HeaderMenu } from './HeaderMenu';
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 export const Header: FC = () => {
   return (
     <StyledHeader>
-      <LogoutButton />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
   );
 };
