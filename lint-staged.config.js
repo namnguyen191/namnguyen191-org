cfb2050822658999273f8cfc2f1cc5c78fe6af68
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-03-10 15:18:03 -0400

    feat: update lint stage to build affected lib

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 const config = {
   'package.json': ['pnpm typecheck:all', 'pnpm lint:all', 'pnpm build:all'],
   '{apps,libs,tools}/**/*.{ts,tsx}': 'pnpm type-check:affected',
-  '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json,html}': 'pnpm build:affected',
+  '{apps,libs,tools}/**': 'pnpm build:affected',
   '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json,html}': [
     (files) => `pnpm nx affected:lint --files=${files.join(',')}`,
     (files) => `pnpm nx format:write --files=${files.join(',')}`,
