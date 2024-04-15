b36cca651f7e3fee9c31a765f0916cb6402b58c9
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-04-14 21:29:01 -0400

    feat: skaffold ui storage asset and add live reload

--- apps/ui-storage/main.go
+++ apps/ui-storage/main.go
@@ -1,12 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
 
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
-}
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println(Hello("ui-storage"))
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		dat, err := os.ReadFile("./assets/remote-resources/boredapi-remote-resource.json")
+
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusOK, gin.H{
+				"message": "error getting your shit",
+			})
+			return
+		}
+
+		fmt.Println(dat)
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
