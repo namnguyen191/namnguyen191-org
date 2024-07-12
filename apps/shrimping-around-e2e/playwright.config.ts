88e149154decd4cc90cf7426ed3d95b3467fac20
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-07-12 00:45:59 -0400

    feat: skaffold shrimping around

---
+++ apps/shrimping-around-e2e/playwright.config.ts
@@ -0,0 +1,39 @@
+import { workspaceRoot } from '@nx/devkit';
+import { nxE2EPreset } from '@nx/playwright/preset';
+import { defineConfig, devices } from '@playwright/test';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const baseURL = process.env['BASE_URL'] || 'http://localhost:3333';
+
+export default defineConfig({
+  ...nxE2EPreset(__filename, { testDir: './src' }),
+  use: {
+    baseURL,
+    trace: 'on-first-retry',
+  },
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'pnpm exec nx serve shrimping-around',
+    url: 'http://localhost:3333',
+    reuseExistingServer: !process.env['CI'],
+    cwd: workspaceRoot,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
