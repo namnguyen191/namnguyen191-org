a5cd86b604c7966534406b4f23c794a66c021369
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-07-14 14:11:59 -0400

    feat: shrimping around add mod scope and skaffold mui, add manifest and icons

--- apps/shrimping-around/src/app/app.config.ts
+++ apps/shrimping-around/src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(appRoutes)],
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(appRoutes),
+    provideAnimations(),
+  ],
 };
