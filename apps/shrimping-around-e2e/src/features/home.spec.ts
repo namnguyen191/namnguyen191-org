fe0f83f2409c78ace23a8f590ff974cba8a251ac
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-07-19 00:03:40 -0400

    feat: shrimping around use beforeeach in e2e

--- apps/shrimping-around-e2e/src/features/home.spec.ts
+++ apps/shrimping-around-e2e/src/features/home.spec.ts
@@ -1,22 +1,24 @@
 import { expect, test } from '@playwright/test';
 
-test('has home', async ({ page }) => {
-  await page.goto('/');
+test.describe('Home page test', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
 
-  await expect(page.locator('h1')).toHaveText('Welcome to home component');
-});
+  test('has home', async ({ page }) => {
+    await expect(page.locator('h1')).toHaveText('Welcome to home component');
+  });
 
-test('has navbar', async ({ page }) => {
-  await page.goto('/');
+  test('has navbar', async ({ page }) => {
+    const navBar = page.getByRole('navigation');
+    await expect(navBar).toBeVisible();
 
-  const navBar = page.getByRole('navigation');
-  await expect(navBar).toBeVisible();
+    await page.goto('/some-other-page');
+    await expect(page).toHaveURL('/some-other-page');
 
-  await page.goto('/some-other-page');
-  await expect(page).toHaveURL('/some-other-page');
-
-  const homeLink = navBar.getByRole('link');
-  await expect(homeLink).toBeVisible();
-  await homeLink.click();
-  await expect(page).toHaveURL('/');
+    const homeLink = navBar.getByRole('link');
+    await expect(homeLink).toBeVisible();
+    await homeLink.click();
+    await expect(page).toHaveURL('/');
+  });
 });
