8e51081805a88a99276548fdb316105273fb3da2
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-07-20 13:08:35 -0400

    feat: migrate to latest dependencies

--- apps/use-popcorn/cypress/support/app.po.ts
+++
@@ -1,5 +0,0 @@
-export const getGreeting = (): Cypress.Chainable<JQuery<HTMLHeadingElement>> =>
-  cy.get('h1').should('have.length', 1);
-export const getSecretModal = (): Cypress.Chainable<JQuery<HTMLDialogElement>> => cy.get('dialog');
-export const getSearchBox = (): Cypress.Chainable<JQuery<HTMLElement>> => cy.get('.search');
-export const getMoviesList = (): Cypress.Chainable<JQuery<HTMLElement>> => cy.get('ul.list');
