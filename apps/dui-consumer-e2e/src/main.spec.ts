fa1cf08dfb510a428e140750ca6faaf79008cd11
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-08-11 01:56:18 -0400

    feat: dui update unit test and carbon table no data class

--- apps/dui-consumer-e2e/src/main.spec.ts
+++ apps/dui-consumer-e2e/src/main.spec.ts
@@ -5,13 +5,22 @@ test.describe('Home page test', () => {
     await page.goto('/dui-e2e');
   });
 
-  test('has carbon table v1', async ({ page }) => {
-    await expect(page.locator('[id="table-header"]')).toHaveText('This is a different table');
+  test.describe('should dynamically change ui', () => {
+    test('has carbon table v1', async ({ page }) => {
+      await expect(page.locator('[id="table-header"]').first()).toHaveText(
+        'This is a different table'
+      );
+    });
+
+    test('has carbon table v2', async ({ page }) => {
+      await expect(page.locator('[id="table-header"]').first()).toHaveText('Carbon test table', {
+        timeout: 10000,
+      });
+    });
   });
 
-  test('has carbon table v2', async ({ page }) => {
-    await expect(page.locator('[id="table-header"]')).toHaveText('Carbon test table', {
-      timeout: 10000,
-    });
+  test('should not show no data for table with loading state', async ({ page }) => {
+    await expect(page.locator('[id="table-header"]')).toHaveCount(3);
+    expect(await page.locator('.no-data').count()).toBe(0);
   });
 });
