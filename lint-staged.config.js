b3f4a287725912aaf04651cb210950f3027f5ad6
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-08-30 23:20:39 -0400

    feat: add stylelint and small adjust to carbon text card styles

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,13 @@
 const config = {
-  'package.json': ['pnpm typecheck:all', 'pnpm lint:all', 'pnpm build:all'],
+  'package.json': ['pnpm typecheck:all', 'pnpm lint:all', 'pnpm stylelint:all', 'pnpm build:all'],
   '{apps,libs,tools}/**/*.{ts,tsx}': 'pnpm type-check:affected',
+  '{apps,libs,tools}/**/*.{css,scss}': 'pnpm stylelint:affected',
   '{apps,libs,tools}/**': 'pnpm build:affected',
   '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json,html}': [
     (files) => `pnpm nx affected:lint --files=${files.join(',')}`,
+    (files) => `pnpm nx format:write --files=${files.join(',')}`,
+  ],
+  '{apps,libs,tools}/**/*.{css,scss}': [
     (files) => `pnpm nx format:write --files=${files.join(',')}`,
   ],
 };
