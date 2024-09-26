ae9fcb563b4709dfbace32e2e1fbfd6487a55b00
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-09-25 23:13:00 -0400

    feat: delete obsolete projects

--- apps/shrimping-around-e2e/src/other-pages/not-found.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('has not found', async ({ page }) => {
-  await page.goto('/some-random-url');
-
-  await expect(page.locator('span')).toHaveText("The page you're looking for is not found");
-});
