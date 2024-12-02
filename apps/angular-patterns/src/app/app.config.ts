ca4b76ccdb6582c0ab60ffc4fc79b031bb628572
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-12-02 10:27:59 -0500

    feat: remove unused deps

--- apps/angular-patterns/src/app/app.config.ts
+++ apps/angular-patterns/src/app/app.config.ts
@@ -3,13 +3,11 @@ import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { globalDelayInterceptorFactory } from '@namnguyen191/common-angular-helper';
-import { provideMonacoEditor } from 'ngx-monaco-editor-v2';
 
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideMonacoEditor(),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(
       appRoutes,
